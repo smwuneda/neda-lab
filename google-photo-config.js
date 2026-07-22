0bb0eae201b8a4411d103e583ceb830b29b0db66
smwuneda <smwu.neda.lab@gmail.com>
2026-07-22 11:59:04 +0900

    Add files via upload

---
+++ google-photo-config.js
@@ -0,0 +1,7 @@
+// Paste the deployed Google Apps Script web app URL between the quotes.
+// You get this URL after: Deploy > New deployment > Web app > Deploy.
+window.NEDA_GOOGLE_PHOTO_WEBAPP_URL = 'https://script.google.com/macros/s/AKfycbyfgf67t2-HbaRt2bJ6PcY4iGwsdIRjJK7u8qNUx71IteiaPgIVSG3FQcRUOomplwM/exec';
+
+// No password is stored here on purpose. Whoever clicks Add/Edit/Delete
+// on the website will be asked to type the admin password each time
+// (it is checked against the Settings sheet, not against this file).
